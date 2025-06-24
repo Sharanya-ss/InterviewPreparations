5f43923c1f3e9992897839366a2a9ebefe7e8ea4
AntonyAkash <antoney97@gmail.com>
2025-06-24 16:18:58 +0530

    class components

---
+++ FrontEnd React Js/1.functional_components/src/components/Classcomponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Classcomponent extends Component {
+  render() {
+    return (
+      <div>Classcomponent</div>
+    )
+  }
+}
+
+export default Classcomponent
