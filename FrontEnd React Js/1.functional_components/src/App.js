ee4e5b66a28e0fb59c737313fd1588fac783ba86
AntonyAkash <antoney97@gmail.com>
2025-06-24 09:12:49 +0530

    react tutorials

---
+++ FrontEnd React Js/1.functional_components/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Child from './components/Child';
+
+function App() {
+  return (
+    <div className="App-header">
+      <label>Priyanka</label>
+      <Child/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd React Js/1.functional_components/src/components/Child.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Child() {
+  return (
+    <div>
+      <h1> Priyanka Hello, World!</h1>
+    </div>
+  )
+}
+
+export default Child
